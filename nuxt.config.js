18547debfd28588d0f13991a9cee3d4a34aec4b7
Shunji Zhan <shunhahaha@live.com>
2021-06-27 11:21:23 +0800

    basic login layout

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,15 +3,24 @@ export default {
     extendRoutes(routes, resolve) {
       routes.splice(0);   // 清空基于pages/默认生成的路由表规则，方便重写
       routes.push(...[{
-        name: '/',
-        path: '*',
+        name: 'home',
+        path: '/',
         component: resolve(__dirname, 'pages/layout'),
         children: [
           {
             path: '', // 默认子路由
-            name: 'home',
             component: resolve(__dirname, 'pages/home/'),
-          }
+          },
+          {
+            name: 'login',
+            path: '/login', 
+            component: resolve(__dirname, 'pages/login/'),
+          },
+          {
+            name: 'register',
+            path: '/register', 
+            component: resolve(__dirname, 'pages/login/'),
+          },
         ],
       }])
     }
