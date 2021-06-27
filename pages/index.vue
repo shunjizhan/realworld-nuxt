9c5287ce977d3f05a55aab0c002bea287ae89efc
Shunji Zhan <shunhahaha@live.com>
2021-06-27 10:29:32 +0800

    able to render layout template

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="">首页</div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mpunted() {},
+  methods: {},
+}
+</script>
+
+<style>
+
+</style>
