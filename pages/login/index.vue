71b7165a4812c446b47993f0490c68ca66a2cd1b
Shunji Zhan <shunhahaha@live.com>
2021-06-27 15:52:07 +0800

    1) able to register and login
    - 基于axois包装了login()和register()
    - 在login/index.vue里用v-model绑定当前的用户数据，加上了基本的表单验证
    - 还有一些基本的错误处理，把错误信息同步到UI上

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -11,23 +11,50 @@
         </p>
 
         <ul class="error-messages">
-          <li>That email is already taken</li>
+          <template v-for="(errDetails, errName) in errors">
+            <li
+              v-for="(e, idx) in errDetails"
+              :key="idx"
+            >
+              {{ errName }}{{ e }}
+            </li>
+          </template>
         </ul>
 
-        <form>
+        <form @submit.prevent="onSubmit">
           <fieldset v-if="!isLogin" class="form-group">
-            <input class="form-control form-control-lg" type="text" placeholder="Your Name">
+            <input 
+              type="text"
+              v-model="user.username"
+              class="form-control form-control-lg"
+              placeholder="Your Name"
+              required
+            >
           </fieldset>
           <fieldset class="form-group">
-            <input class="form-control form-control-lg" type="text" placeholder="Email">
+            <input
+              type="email"
+              v-model="user.email" 
+              class="form-control form-control-lg"
+              placeholder="Email"
+              required
+            >
           </fieldset>
           <fieldset class="form-group">
-            <input class="form-control form-control-lg" type="password" placeholder="Password">
+            <input
+              type="password"
+              v-model="user.password"
+              class="form-control form-control-lg"
+              placeholder="Password"
+              minLength="8"
+              required
+            >
           </fieldset>
           <button class="btn btn-lg btn-primary pull-xs-right">
             {{ isLogin ? 'Sign in' : 'Sign up' }}
           </button>
         </form>
+
       </div>
 
     </div>
@@ -36,11 +63,44 @@
 </template>
 
 <script>
+import {
+  login,
+  register,
+} from '@/api/user';
+
 export default {
   name: 'LoginIndex',
   computed: {
-    isLogin() {
+    isLogin () {
       return this.$route.name === 'login'
+    },
+  },
+  data () {
+    return {
+      user: {
+        username: '',
+        email: '',
+        password: '',
+      },
+      errors: {},
+    }
+  },
+  methods: {
+    async onSubmit () {
+      try {
+        const { data } = this.isLogin
+          ? await login({ user: this.user })
+          : await register({ user: this.user })
+  
+        // TODO: 保存用户登陆状态
+  
+        // 跳转回首页
+        // TODO: 为什么这里是$router，上面的$route
+        // TODO: 为什么有些事key（比如computed，methods），有些是method，比如data（）
+        this.$router.push('/');        
+      } catch (e) {
+        this.errors = e.response.data.errors;
+      }
     }
   }
 }
